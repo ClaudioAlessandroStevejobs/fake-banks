e1156e07dc8205cde66a08a5e936c50ae5496f63
ClaudioAlessandroStevejobs <claudio.alessandro@stevejobs.academy>
2021-02-03 11:23:37 +0100

    fix error validationMiddleware

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -3,5 +3,5 @@ import {validationResult} from 'express-validator';
 // ?? errors
 export const validationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
-    errors.array().length > 0 ? res.status(404).json(errors) : next();
+    errors.array().length > 0 ? res.status(400).json(errors) : next();
 }
